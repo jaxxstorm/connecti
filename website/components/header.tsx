41cdae536f3859aa0e6bf5fd873ac5d7bce318e3
zchase <zachary@pulumi.com>
2022-10-26 13:35:21 -0700

    Add github link

--- website/components/header.tsx
+++ website/components/header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import Script from "next/script";
 
 export const Header: React.FC = ({}) => {
     return(
-        <header className="w-full">
-            <div className="flex p-6 bg-gray-900">
+        <header className="w-full relative">
+            <div className="flex py-8 p-6 bg-gray-900">
                 <div className="w-1/3 lg:w-auto flex items-center justify-center pr-12">
                     <a href="/" className="flex justify-center items-center">
                         <div className="pr-2">
@@ -26,6 +27,18 @@ export const Header: React.FC = ({}) => {
                     <a href="/#get-started" className="rounded bg-gray-700 px-6 py-3 text-white font-bold hover:bg-gray-600">Get Started</a>
                 </div>
             </div>
+            <div className="absolute top-1 right-2">
+                <a
+                    className="github-button"
+                    href="https://github.com/jaxxstorm/connecti"
+                    data-size="small"
+                    data-show-count="true"
+                    aria-label="Star jaxxstorm/connecti on GitHub"
+                >
+                    Star
+                </a>
+            </div>
+            <Script async defer src="https://buttons.github.io/buttons.js" />
         </header>
     );
 };
